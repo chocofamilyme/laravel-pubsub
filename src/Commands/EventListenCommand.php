6c52644b0729f5478c89ead60e6323a7394125d5
moldabayev.v <moldabayev.v@chocolife.kz>
2020-11-02 13:37:16 +0600

    Fix rabbitmq:consume name option does not exist

--- src/Commands/EventListenCommand.php
+++ src/Commands/EventListenCommand.php
@@ -10,6 +10,7 @@ class EventListenCommand extends ConsumeCommand
     protected $signature = 'event:listen
                             {event? : Event name, e.g. user.# -> listen to all events starting with user.}
                             {connection=rabbitmq : The name of the queue connection to work}
+                            {--name=default : The name of the consumer}
                             {--queue= : The names of the queues to work}
                             {--exchange= : Optional, specifies exchange which should be listened [for default value see app/config/queue.php]}
                             {--exchange_type=topic : Optional, specifies exchange which should be listened [for default value see app/config/queue.php]}
