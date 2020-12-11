b41125c3a1cc417b6722f0cf9e3326cae84356af
Aidyn Makhataev <aidyn@rahmetapp.kz>
2020-12-11 15:11:48 +0600

    add config pubsub

---
+++ tests/config/pubsub.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Listen for events
+    |--------------------------------------------------------------------------
+    |
+    | Define event name and it's listeners. Please notice that one event name may have multiple listeners
+    |
+    | Example:
+    |
+    | listen => [
+    |     'UserNotified' => [
+    |         NotifyAboutDeviceChangeListener::class,
+    |     ]
+    | ],
+    |
+    */
+    'listen' => [],
+
+    /**
+     * Define database tables for storing data (publishing events, incoming events, etc.)
+     */
+    'tables' => [
+        'events' => 'pubsub_events',
+    ],
+];
