a2bab2a9499e49f66f38e2ea8dc3c461f5e397aa
moldabayev.v <moldabayev.v@chocolife.kz>
2020-12-31 12:18:53 +0600

    v6 alfa

--- src/Queue/RabbitMQQueue.php
+++ src/Queue/RabbitMQQueue.php
@@ -2,8 +2,6 @@
 
 declare(strict_types=1);
 
-/** @noinspection PhpRedundantCatchClauseInspection */
-
 namespace Chocofamilyme\LaravelPubSub\Queue;
 
 use Chocofamilyme\LaravelPubSub\Message\OutputMessage;
